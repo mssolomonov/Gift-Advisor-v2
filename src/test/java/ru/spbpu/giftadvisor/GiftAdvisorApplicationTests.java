019b200f9d57cc4b6e611299a49f32cdfd127ea8
shemya <anastasia.shemyakinskaya@emc.com>
2019-10-31 15:14:06 +0300

    Added gradle project

---
+++ src/test/java/ru/spbpu/giftadvisor/GiftAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbpu.giftadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
