82a3d507b541cdc27cfc3bc562f10569aec877ed
Maxim_Solomonov <maxim.bannik@yandex.ru>
2020-05-17 18:14:53 +0300

    Test for test

--- gift-advisor/e2e/src/addgift/add.e2e-spec.ts
+++ gift-advisor/e2e/src/addgift/add.e2e-spec.ts
@@ -56,4 +56,64 @@ describe('Add page e2e', () => {
     searchPage.getLogoutButton().click();
   });
 
+  it('should login and can not delete gift of another user', async () => {
+    await page.navigateTo();
+
+    page.getUsernameInput().sendKeys("shemya123");
+    page.getPasswordInput().sendKeys("12345678");
+    page.getLoginButton().click();
+
+    searchPage.getMenuButton().click();
+    searchPage.geAddNewGiftButton().click();
+
+    addPage.getNameInput().sendKeys("name");
+    await addPage.getPriceInput().clear().then();
+    addPage.getPriceInput().sendKeys("1.01");
+    addPage.getDescriptionInput().sendKeys("13231kljljfsdf");
+    addPage.getChipList().sendKeys("cup");
+    addPage.getMatOption().click();
+
+    addPage.geSubmitButton().click();
+
+    searchPage.getMenuButton().click();
+    searchPage.getLogoutButton().click();
+
+    page.getUsernameInput().sendKeys("solomonka");
+    page.getPasswordInput().sendKeys("87654321");
+    page.getLoginButton().click();
+
+    await searchPage.navigateTo();
+    await searchPage.getListElements().then(function(gifts) {
+        let matline = gifts[gifts.length-1].all(by.className("mat-line"));
+        let name = matline.get(0).getText();
+        let desc = matline.get(1).getText();
+        let price = matline.get(2).getText();
+        expect(name).toEqual("name");
+        expect(desc).toEqual("13231kljljfsdf");
+        expect(price).toEqual("1.01$");
+        matline.get(0).click();
+    });
+
+    expect(addPage.getNameInput().getAttribute('value')).toContain("name");
+    expect(addPage.getDescriptionInput().getAttribute('value')).toContain("13231kljljfsdf");
+    expect(addPage.getPriceInput().getAttribute('value')).toContain("1.01");
+    expect(addPage.getMatChip().count()).toEqual(1);
+    expect(addPage.getMatChip().get(0).getText()).toContain("cup");
+    expect(!addPage.getDeleteButton().isPresent());
+
+    searchPage.getMenuButton().click();
+    searchPage.getLogoutButton().click();
+
+    page.getUsernameInput().sendKeys("shemya123");
+    page.getPasswordInput().sendKeys("12345678");
+    page.getLoginButton().click();
+
+    addPage.getDeleteButton().click();
+    element(by.buttonText("Yes")).click();
+
+    searchPage.getMenuButton().click();
+    searchPage.getLogoutButton().click();
+
+  });
+
 });
